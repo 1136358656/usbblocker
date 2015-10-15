5c965c0f8e04997e4afc57903e8464302e705d09
Kaguya <kaguya@ganxiang.wang>
2015-10-15 22:49:10 +0800

    init

---
+++ USBBlocker/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace USBBlocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckDeviceStatus_Lable = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CheckDeviceStatus_Lable
+            // 
+            this.CheckDeviceStatus_Lable.AutoSize = true;
+            this.CheckDeviceStatus_Lable.Location = new System.Drawing.Point(27, 28);
+            this.CheckDeviceStatus_Lable.Name = "CheckDeviceStatus_Lable";
+            this.CheckDeviceStatus_Lable.Size = new System.Drawing.Size(55, 15);
+            this.CheckDeviceStatus_Lable.TabIndex = 0;
+            this.CheckDeviceStatus_Lable.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.CheckDeviceStatus_Lable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CheckDeviceStatus_Lable;
+    }
+}
+
---
+++ USBBlocker/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Threading;
+using System.Security.Permissions;
+
+namespace USBBlocker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public enum DeviceEvent : int
+        {
+            DBT_CONFIGCHANGECANCELED = 0x0019,
+            DBT_CONFIGCHANGED = 0x0018,
+            DBT_CUSTOMEVENT = 0x8006,
+            DBT_DEVICEARRIVAL = 0x8000,//USB Insert DEvice Statu
+            DBT_DEVICEQUERYREMOVE = 0x8001,
+            DBT_DEVICEQUERYREMOVEFAILED = 0x8002,
+            DBT_DEVICEREMOVEPENDING = 0x8003,//USB Revoing.
+            DBT_DEVICEREMOVECOMPLETE = 0x8004,//USB Remove Completed
+            DBT_DEVICETYPESPECIFIC = 0x8005,
+            DBT_DEVNODES_CHANGED = 0x0007,//Device List _Changed
+            DBT_QUERYCHANGECONFIG = 0x0017,
+            DBT_USERDEFINED = 0xFFFF
+        }
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            DeviceEvent lEvent;
+
+            lEvent = (DeviceEvent)m.WParam.ToInt32();
+            switch (lEvent)
+            {
+                case DeviceEvent.DBT_DEVICEARRIVAL://[Insert]
+                    this.CheckDeviceStatus_Lable.BackColor = Color.Green;
+                    this.CheckDeviceStatus_Lable.Text = "----Connection Device!----";
+                    MessageBox.Show("Just Insert At Moment !", "Insert");
+                    break;
+                case DeviceEvent.DBT_DEVICEREMOVECOMPLETE://[REmove]
+                    this.CheckDeviceStatus_Lable.BackColor = Color.Red;
+                    this.CheckDeviceStatus_Lable.Text = "------No Connection------";
+                    MessageBox.Show("Remove Complete At Moment!", "Remove");
+                    break;
+                case DeviceEvent.DBT_DEVNODES_CHANGED://[Device List Have Changed]
+                    MessageBox.Show("Device List have been Changed!");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
